1f37a2aa56f3e1c740c6edd2931bea1250568fd9
Kulikova Darya <daryakulikova2206@mail.ru>
2025-06-11 01:41:28 +0300

    1

--- src/app/(Main)/courses/page.tsx
+++ src/app/(Main)/courses/page.tsx
@@ -1,9 +1,17 @@
 import styles from "./coursesPage.module.scss";
 import FilterCoursesClient from "@/widget/FilterCourseClient/FilterCoursesClient";
 import { CourseService } from "@/services/course.service";
+import { Course } from "@/shared/types/course.types";
 
 export default async function CoursesPage() {
-  const initialCourses = await CourseService.courseAll();
+  let initialCourses: Course[] = [];
+
+  try {
+    const response = await CourseService.courseAll();
+    initialCourses = Array.isArray(response) ? response : [];
+  } catch (error) {
+    console.error("Ошибка при получении курсов:", error);
+  }
 
   return (
     <div className={styles["courses"]}>
